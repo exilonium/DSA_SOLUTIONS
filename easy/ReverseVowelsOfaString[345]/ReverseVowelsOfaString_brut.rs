e6474f087a306d755324d122ad2ff34d439221f9
exilonium <exilonium@proton.me>
2025-12-11 13:03:44 +0530

    345. Reverse Vowel of a String (brut)

---
+++ easy/ReverseVowelsOfaString[345]/ReverseVowelsOfaString_brut.rs
@@ -0,0 +1,28 @@
+//brut highly inefficient
+impl Solution {
+    pub fn reverse_vowels(s: String) -> String {
+        let mut inpstr: Vec<char> = s.chars().collect();
+        if inpstr.len() <= 1 {
+            return s;
+        }
+        let mut low: usize = 0;
+        let mut high: usize = inpstr.len() - 1;
+
+        let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+        while low < high {
+            while low < high && !vowels.contains(&inpstr[low]) {
+                low += 1;
+            }
+            while low < high && !vowels.contains(&inpstr[high]) {
+                high -= 1;
+            }
+            if low < high {
+                inpstr.swap(high, low);
+                low += 1;
+                high -= 1;
+            }
+        }
+        inpstr.iter().collect()
+    }
+}
