25cf5ad0abe377f8351348b64f2f17249318a490
exilonium <exilonium@proton.me>
2025-12-14 00:03:55 +0530

    371. Sum Of Two Integers

---
+++ medium/SumOfTwoIntegers[371]/SumOfTwoIntegers.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn get_sum(a: i32, b: i32) -> i32 {
+        let mut sum = a;
+        let mut carry = b;
+
+        while carry != 0 {
+            let temp = sum ^ carry;
+            carry = (sum & carry) << 1;
+            sum = temp;
+        }
+
+        sum
+    }
+}
