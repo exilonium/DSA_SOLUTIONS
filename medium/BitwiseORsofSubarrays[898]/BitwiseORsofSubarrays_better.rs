a31b51ba650d77fc80a13caa5035edad4672ca6e
exilonium <exilonium@proton.me>
2025-12-21 22:58:41 +0530

    898. BitwiseORsofSubarrays (better)

---
+++ medium/BitwiseORsofSubarrays[898]/BitwiseORsofSubarrays_better.rs
@@ -0,0 +1,18 @@
+// brut
+// TLE 78/87 cases passed
+use std::collections::HashMap;
+impl Solution {
+    pub fn subarray_bitwise_o_rs(arr: Vec<i32>) -> i32 {
+        let mut map = HashMap::new();
+        for i in 0..arr.len() {
+            let mut or = 0;
+            for j in i..arr.len() {
+                or |= arr[j];
+                map.entry(or)
+                    .and_modify(|counter| *counter += 1)
+                    .or_insert(1);
+            }
+        }
+        map.len() as _
+    }
+}
