6037ddab4c4d77d6e0abc0224e6393930d515464
exilonium <exilonium@proton.me>
2025-12-22 23:23:02 +0530

    3. Longest Substring Without Repeating Characters

---
+++ medium/LongestSubstringWithoutRepeatingCharacters[3]/LongestSubstringWithoutRepeatingCharacters.rs
@@ -0,0 +1,16 @@
+// optimal
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut max_length = 0;
+        let mut last_index = [0; 128];
+        let mut start = 0;
+
+        for (end, ch) in s.chars().enumerate() {
+            start = start.max(last_index[ch as usize]);
+            max_length = max_length.max(end - start + 1);
+            last_index[ch as usize] = end + 1;
+        }
+
+        max_length as i32
+    }
+}
