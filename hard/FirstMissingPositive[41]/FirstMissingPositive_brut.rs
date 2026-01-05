2f964e12fd961505df590b4d8b35f83b41cc4874
exilonium <exilonium@proton.me>
2026-01-06 00:07:58 +0530

    41. First Missing Positive (garbage code brut)

---
+++ hard/FirstMissingPositive[41]/FirstMissingPositive_brut.rs
@@ -0,0 +1,21 @@
+// this is a garbage code but still im putting it here
+// brut
+impl Solution {
+    pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
+        let mut low = 0;
+        let mut found = -1;
+        for i in 1..i32::MAX {
+            for j in &nums {
+                if i == *j {
+                    found = 1;
+                    continue;
+                }
+            }
+            if found == -1 {
+                return i;
+            }
+            found = -1;
+        }
+        return -1;
+    }
+}
