39e9f373a81228b10d60bcc22c7669ff46231abf
exilonium <exilonium@proton.me>
2026-01-10 23:55:04 +0530

    2859. Sum of Values at Indices With K Set Bits (better)

---
+++ easy/SumofValuesatIndicesWithKSetBits[2859]/SumofValuesatIndicesWithKSetBits_better.rs
@@ -0,0 +1,20 @@
+// 0ms but it can be improved
+impl Solution {
+    pub fn sum_indices_with_k_set_bits(nums: Vec<i32>, k: i32) -> i32 {
+        let mut res = 0;
+        for i in 0..nums.len() {
+            let mut count = 0;
+            let mut idx = i;
+            for _ in 0..31 {
+                if idx & 1 == 1 {
+                    count += 1;
+                }
+                idx = idx >> 1;
+            }
+            if count == k {
+                res += nums[i];
+            }
+        }
+        res
+    }
+}
