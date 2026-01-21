323479f8947e074ceed1032a60009f1bc0e05915
exilonium <exilonium@proton.me>
2026-01-21 22:14:33 +0530

    142. Linked List Cycle II

---
+++ medium/LinkedListCycleII[142]/LinkedListCycleII.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        found = False
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                found = True
+                break
+        if not found:
+            return None
+        slow = head
+        while fast != slow:
+            slow = slow.next
+            fast = fast.next
+        return slow
