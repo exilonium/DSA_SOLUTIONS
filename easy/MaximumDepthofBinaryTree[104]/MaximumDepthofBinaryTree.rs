cf8073221c1685554264aea13937fda81d499e04
exilonium <exilonium@proton.me>
2026-02-14 22:49:05 +0530

    104. Maximum Depth of Binary Tree

---
+++ easy/MaximumDepthofBinaryTree[104]/MaximumDepthofBinaryTree.rs
@@ -0,0 +1,32 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                let maxL = Self::max_depth(node.borrow().left.clone());
+                let maxR = Self::max_depth(node.borrow().right.clone());
+                return (std::cmp::max(maxL, maxR) + 1);
+            }
+            None => 0,
+        }
+    }
+}
