b1575713718e1f262076a2f7c4f85ee7d985b39f
exilonium <exilonium@proton.me>
2026-02-25 23:32:09 +0530

    342. Power of Four

---
+++ easy/PowerofFour[342]/PowerofFour_brut.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn is_power_of_four(n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+        while n % 4 == 0 {
+            n /= 4;
+        }
+        n == 1
+    }
+}
