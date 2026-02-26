dff96b16ade0331d19db721fcf698563d7a36bb8
exilonium <exilonium@proton.me>
2026-02-26 23:42:18 +0530

    145. Binary Tree Postorder Traversal

---
+++ easy/BinaryTreePostorderTraversal[145]/BinaryTreePostorderTraversal.rs
@@ -0,0 +1,37 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = vec![];
+        fn postorder(root: Option<Rc<RefCell<TreeNode>>>, res: &mut Vec<i32>) {
+            if let Some(node) = root {
+                let n = node.borrow();
+                let l = n.left.clone();
+                let r = n.right.clone();
+                postorder(l, res);
+                postorder(r, res);
+                res.push(n.val)
+            };
+        }
+        postorder(root, &mut res);
+        res
+    }
+}
