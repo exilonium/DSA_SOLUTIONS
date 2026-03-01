ecd6ff8e5c0ce216555c4df5ccc19383979b4b6e
exilonium <exilonium@proton.me>
2026-03-01 22:50:36 +0530

    693. Binary Number with Alternating Bits (iterative)

---
+++ easy/BinaryNumberwithAlternatingBits[693]/BinaryNumberwithAlternatingBits.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn has_alternating_bits(n: i32) -> bool {
+        let mut n = n;
+        let mut prev = n & 1;
+        n >>= 1;
+        while n > 0 {
+            let curr = n & 1;
+            if curr == prev {
+                return false;
+            }
+            prev = curr;
+            n >>= 1;
+        }
+        true
+    }
+}
