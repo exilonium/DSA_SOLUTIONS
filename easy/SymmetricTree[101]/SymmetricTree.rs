b0c3714cf68e5989579ce4b6231603be9db76d82
exilonium <exilonium@proton.me>
2026-03-17 22:54:25 +0530

    101.Symmetric Tree

---
+++ easy/SymmetricTree[101]/SymmetricTree.rs
@@ -0,0 +1,49 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        type NODE = Option<Rc<RefCell<TreeNode>>>;
+
+        fn helper(left: NODE, right: NODE) -> bool {
+            match (left, right) {
+                (None, None) => true,
+                (Some(l), Some(r)) => {
+                    let l_ref = l.borrow();
+                    let r_ref = r.borrow();
+
+                    l_ref.val == r_ref.val
+                        && helper(l_ref.left.clone(), r_ref.right.clone())
+                        && helper(l_ref.right.clone(), r_ref.left.clone())
+                }
+                _ => false,
+            }
+        }
+
+        match root {
+            None => true,
+            Some(node) => {
+                let node_ref = node.borrow();
+                helper(node_ref.left.clone(), node_ref.right.clone())
+            }
+        }
+    }
+}
