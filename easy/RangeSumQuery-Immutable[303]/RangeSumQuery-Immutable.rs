fd7547da40a0d0e639114cc3ddd4d5df3114aa31
exilonium <exilonium@proton.me>
2026-04-06 19:38:47 +0530

    303. Range Sum Query - Immutable

---
+++ easy/RangeSumQuery-Immutable[303]/RangeSumQuery-Immutable.rs
@@ -0,0 +1,30 @@
+struct NumArray {
+    prev_sum : Vec<i32>,
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumArray {
+
+    fn new(nums: Vec<i32>) -> Self {
+        let size = nums.len();
+        let mut prev_sum = vec![0;size+1];
+        for i in 0..size{
+            prev_sum[i+1] = prev_sum[i] + nums[i];
+        }
+        Self { prev_sum }
+    }
+    
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        self.prev_sum[right as usize +1] - self.prev_sum[left as usize] // trick fahh
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * let obj = NumArray::new(nums);
+ * let ret_1: i32 = obj.sum_range(left, right);
+ */
