6621628edd85339518f29408a1dcede14074a4af
exilonium <exilonium@proton.me>
2026-05-20 22:24:14 +0530

    665. Non-decreasing Array

---
+++ medium/Non-decreasingArray[665]/Non-decreasingArray.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn check_possibility(nums: Vec<i32>) -> bool {
+        let mut count = 0;
+        let mut prev = std::i32::MIN;
+        for i in 0..nums.len() {
+            if nums[i] < prev {
+                count += 1;
+                if count > 1 {
+                    return false;
+                }
+                if i > 1 && nums[i - 2] > nums[i] {
+                    continue;
+                }
+            }
+            prev = nums[i];
+        }
+        true
+    }
+}
