cdf6fd8bf481c33dd94671a0a50a28c2b72d53c9
exilonium <exilonium@proton.me>
2026-06-12 23:01:19 +0530

    700. Search in a Binary Search Tree

---
+++ easy/SearchinaBinarySearchTree[700]/SearchinaBinarySearchTree.rs
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn search_bst(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = &root {
+            let curr = node.borrow();
+            if curr.val < val {
+                return Self::search_bst(curr.right.clone(), val);
+            } else if curr.val > val {
+                return Self::search_bst(curr.left.clone(), val);
+            }
+        }
+        root
+    }
+}
