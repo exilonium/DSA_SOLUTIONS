d6577a58137adef22735b16c641805da35801e77
exilonium <exilonium@proton.me>
2026-09-24 23:57:52 +0530

    1470. Shuffle the Array

---
+++ easy/ShuffletheArray[1470]/ShuffletheArray.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let n = n as usize;
+        let mut ans = Vec::with_capacity(nums.len());
+
+        for i in 0..n {
+            ans.push(nums[i]);
+            ans.push(nums[i + n]);
+        }
+
+        ans
+    }
+}
